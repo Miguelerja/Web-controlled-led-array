4c40c6ccce1c3046caec9a6e96744372b656421f
Miguelerja <miguelerja@gmail.com>
2020-01-11 19:39:42 +0100

    add basic html and css

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const PORT = 3000;
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
