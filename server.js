4a918cb87dbd84160e76140ea1359d2dce494c87
Miguelerja <miguelerja@gmail.com>
2020-01-12 19:10:04 +0100

    refactor server

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
 const express = require('express');
-const { Board, Led } = require('johnny-five');
+const { Board, Led, Leds } = require('johnny-five');
 const { EVENTS, PORT } = require('./vars');
+
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
-const board = new Board({
-  repl: false,
-});
+const board = new Board({ repl: false });
 
 board.on('ready', () => {
-  const whiteLed = new Led({
-    pin: 10,
-  });
-  const redLed = new Led({
-    pin: 6,
-  });
-  const yellowLed = new Led({
-    pin: 5,
-  });
-  const blueLed = new Led({
-    pin: 9,
-  });
-  const greenLed = new Led({
-    pin: 3,
-  });
+  const whiteLed = new Led(10);
+  const redLed = new Led(6);
+  const yellowLed = new Led(5);
+  const blueLed = new Led(9);
+  const greenLed = new Led(3);
+
+  const lights = new Leds([greenLed, blueLed, yellowLed, redLed, whiteLed]);
 
   io.on('connection', (socket) => {
     console.log('socket connected');
-    greenLed.on();
-    blueLed.on();
-    yellowLed.on();
-    redLed.on();
-    whiteLed.on();
+    lights.on();
 
     socket.on(EVENTS.GREEN_LED_BRIGHTNESS, (data) => greenLed.brightness(data));
     socket.on(EVENTS.BLUE_LED_BRIGHTNESS, (data) => blueLed.brightness(data));
@@ -41,22 +28,11 @@ board.on('ready', () => {
     socket.on(EVENTS.WHITE_LED_BRIGHTNESS, (data) => whiteLed.brightness(data));
   });
 
-  board.on('exit', () => {
-    greenLed.off();
-    blueLed.off();
-    yellowLed.off();
-    redLed.off();
-    whiteLed.off();
-  });
+  board.on('exit', () => lights.off());
 });
-
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-server.listen(PORT, () => {
-  console.log('Server listening on Port 3000');
-});
+server.listen(PORT, () => console.log('Server listening on Port 3000'));
